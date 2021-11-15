c4bc35e022511e24423584373563c73637b35f2c
Suchandra Jindam <jindam@vizexperts.com>
2021-11-16 00:44:07 +0530

    adding new

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('register/', views.register,name="register"),
