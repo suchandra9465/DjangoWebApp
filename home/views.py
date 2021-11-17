80559d3e3c166d18884ad4ebafb0c43c2855eb86
Suchandra Jindam <jindam@vizexperts.com>
2021-11-17 20:15:10 +0530

    adding data to tables

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages,auth
 from django.contrib.auth.decorators import login_required
-
+from .models import large
 from django.utils import timezone
 
 # Create your views here.
@@ -42,9 +42,34 @@ def migrations(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         group_address = request.POST.get('device_group_template')
+        target_ip = request.POST.get('target_ip')
+        loggingProfileName = request.POST.get('logging_profile_name')
+        securityProfileName = request.POST.get('security_profile_name')
+        interfaceMapping = request.POST.get('interface_mappings')
+        zoneMapping = request.POST.get('zone_mapping')
+        removeDupes = request.POST.get('remove_dupes')
+        if removeDupes == 'on':
+            removeDupes = True
+        else:
+            removeDupes = False
+        removeUnused = request.POST.get('remove_unused')
+        if removeUnused == 'on':
+            removeUnused = True
+        else:
+            removeUnused = False
+        checkPointExpansion = request.POST.get('checkpoint')
+        if checkPointExpansion == 'on':
+            checkPointExpansion = True
+        else:
+            checkPointExpansion = False
+        print(zoneMapping,interfaceMapping,removeDupes,removeUnused,checkPointExpansion)
         messages.success(request, 'Job has launched successfully',extra_tags='alert')
         result = "/opy/pavan.py --grouptar {group} -nexpose AGILE_GROUP --nexposeaddr  'file.csv' --iwtype sw --username {user} --password {pwd}".format(group=group_address, user=username,pwd=password)
         print(result)
+        
+        data_entry = large(createdBy=request.user.username,createdAt=timezone.now(),jobType="migrations",username=username,password=password,targetID=target_ip,loggingProfileName=loggingProfileName,securityProfileName=securityProfileName,interfaceMapping=interfaceMapping,zoneMapping=zoneMapping,removeDupes=removeDupes,removeUnused=removeUnused,checkPointExpansion=checkPointExpansion)
+        data_entry.save()
+        
         return redirect('migrations')
     
     return render(request, 'home/migration_form.html')
@@ -52,15 +77,20 @@ def migrations(request):
 @login_required
 def dumpConfig(request):
     if request.method == 'POST':
+        #getting the values from dumpConfig Form
         username = request.POST.get('username')
         password = request.POST.get('password')
-        group_address = request.POST.get('address_group_name')
-        debug_enable = request.POST.get('comments')
-        print(username,password,group_address,debug_enable)
+        target_ip = request.POST.get('target_ip')
+        debug_enable = request.POST.get('enableCheck')
+        
+        # print(username,password,target_ip,debug_enable,created_by,created_at)
+        
         messages.success(request, 'Job has launched successfully',extra_tags='alert')
-        result = "/opt/scripts/git/m65/m5.py --nexpose DeleteMe --groupadd {group} --fwtype sw65 --grouptargets 10.0.8.237 --username {user} --password {pwd} --comment 'Test'".format(group=group_address, user=username,pwd=password)
+        result = "/opt/scripts/git/m65/m5.py --nexpose DeleteMe --groupadd {group} --fwtype sw65 --grouptargets 10.0.8.237 --username {user} --password {pwd} --comment 'Test'".format(group=target_ip, user=username,pwd=password)
         print(result)
         
+        data_entry = large(createdBy=request.user.username,createdAt=timezone.now(),jobType="dumpConfig",username=username,password=password,targetID=target_ip,enableDebugOutput=debug_enable)
+        data_entry.save()
         
         return redirect('dumpconfig')
     
