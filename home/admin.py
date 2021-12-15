ae1503e3780c3a129d78b0a7149ea035f5809d3b
Suchandra Jindam <jindam@vizexperts.com>
2021-12-15 20:19:08 +0530

    registerd joblogs table to admin

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import large
+from .models import large,jobLogs
 
 # Register your models here.
 admin.site.register(large)
+admin.site.register(jobLogs)
